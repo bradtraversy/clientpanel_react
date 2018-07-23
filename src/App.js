e52a9cb705ceb06e84e921e09594b10d81cb6526
Brad Traversy <traversymedia@gmail.com>
2018-07-23 12:29:04 -0400

    Login functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
 import EditClient from './components/clients/EditClient';
 import ClientDetails from './components/clients/ClientDetails';
+import Login from './components/auth/Login';
 
 import './App.css';
 
@@ -25,6 +26,7 @@ class App extends Component {
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </div>
---
+++ src/components/auth/Login.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { firebase } = this.props;
+    const { email, password } = this.state;
+
+    firebase
+      .login({
+        email,
+        password
+      })
+      .catch(err => alert('Invalid Login Credentials', 'error'));
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="text-center pb-4 pt-3">
+                <span className="text-primary">
+                  <i className="fas fa-lock" /> Login
+                </span>
+              </h1>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="email"
+                    required
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    required
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-primary btn-block"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  firebase: PropTypes.object.isRequired
+};
+
+export default firebaseConnect()(Login);
